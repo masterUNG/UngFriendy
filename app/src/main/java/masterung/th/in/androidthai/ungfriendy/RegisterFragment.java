3ac6a0fd6c62d796ae303dd0af2f7e9c40a9db06
masterUNG <phrombutr@gmail.com>
2018-12-03 14:14:03 +0700

    Create Toolbar Success

--- app/src/main/java/masterung/th/in/androidthai/ungfriendy/RegisterFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/ungfriendy/RegisterFragment.java
@@ -2,7 +2,9 @@ package masterung.th.in.androidthai.ungfriendy;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,22 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
