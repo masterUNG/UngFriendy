fa41ccd82b33d45429f1ee8fb6cb182836286117
masterUNG <phrombutr@gmail.com>
2018-12-04 13:43:21 +0700

    Get Json Finish

--- app/src/main/java/masterung/th/in/androidthai/ungfriendy/MainFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/ungfriendy/MainFragment.java
@@ -4,9 +4,12 @@ package masterung.th.in.androidthai.ungfriendy;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -28,8 +31,62 @@ public class MainFragment extends Fragment {
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
 
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                MyAlert myAlert = new MyAlert(getActivity());
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+                    myAlert.normalDialog(getString(R.string.title_space),
+                            getString(R.string.message_space));
+                } else {
+
+                    checkUserAnPass(userString, passwordString);
+
+                }   // if
+
+
+
+            }
+        });
+    }
+
+    private void checkUserAnPass(String userString, String passwordString) {
+
+        MyAlert myAlert = new MyAlert(getActivity());
+        MyConstant myConstant = new MyConstant();
+        boolean b = true;   // True ==> User False, False ==> User True
+
+        try {
+
+            GetAllDataThread getAllDataThread = new GetAllDataThread(getActivity());
+            getAllDataThread.execute(myConstant.getUrlGetAllUser());
+
+            String jsonString = getAllDataThread.get();
+            Log.d("4DecV2", "jsonString ==> " + jsonString);
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtRegister);
--- app/src/main/java/masterung/th/in/androidthai/ungfriendy/MyConstant.java
+++ app/src/main/java/masterung/th/in/androidthai/ungfriendy/MyConstant.java
@@ -4,6 +4,11 @@ public class MyConstant {
 
     private String urlAddUser = "https://www.androidthai.in.th/hit/addUserMaster.php";
     private String urlPreAvata = "https://androidthai.in.th/hit/MasterUNG";
+    private String urlGetAllUser = "https://androidthai.in.th/hit/getAllUserMaster.php";
+
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
 
     public String getUrlPreAvata() {
         return urlPreAvata;
